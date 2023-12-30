2a193b9c54b1110218e60e2b71df2b84904aa5bf
n3-n2-n1 <xiomaraengine@gmail.com>
2023-12-30 17:24:25 -0300

    ts errors: categories and app {IMPORTANT!: Search Provider Fix undone!!!}

--- dlc-front/src/App.tsx
+++ dlc-front/src/App.tsx
@@ -2,15 +2,15 @@ import { Suspense } from "react";
 import Loader from "./components/Loader/Loader";
 import AppRoutes from "./routes/routes";
 
-import { SearchProvider } from "./contexts/SearchContext";
-
 const App = () => {
   return (
-    <SearchProvider >
+
+    //**Hay que arreglar el search provider que pide un children de alla pero aca tambien, si lo declaras sobreescribe lo que le pasas u.u**// 
+    //<SearchProvider >
       <Suspense fallback={<Loader />}>
         <AppRoutes />
       </Suspense>
-    </SearchProvider>
+    //</SearchProvider>
   );
 };
 
