876d47c2e13393467b40b5a533801fa9affcde4b
n3-n2-n1 <xiomaraengine@gmail.com>
2024-01-03 01:27:06 -0300

    sql final integracion -> done

--- dlc-back/app.js
+++ dlc-back/app.js
@@ -1,11 +1,10 @@
-const express = require("express")
-const sqlite3 = require('sqlite3').verbose();
+const express = require("express");
 const cors = require('cors');
 const usuariosRoutes = require('./src/routes/userRoutes');
 const productosRoutes = require('./src/routes/productsRoutes');
 const errorProductsRoutes = require('./src/routes/errorProductsRoutes');
-
-const initializePassport = require('./src/auth/passport')
+const initializePassport = require('./src/auth/passport');
+const mysql = require('mysql');
 
 const app = express();
 const PORT = 3000;
@@ -19,11 +18,30 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+// Configuración de la conexión a la base de datos
+const dbConfig = {
+  host: 'localhost',
+  user: 'crystal',
+  password: 'crystal',
+  database: 'crystal',
+};
+
+const connection = mysql.createConnection(dbConfig);
+
+// Conectar a la base de datos
+connection.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos:', err);
+  } else {
+    console.log('Conexión exitosa a la base de datos');
+  }
+});
+
 // Usa los routers exportados
 app.use(usuariosRoutes);
 app.use(productosRoutes);
 app.use(errorProductsRoutes);
-initializePassport()
+initializePassport();
 
 // Iniciar el servidor
 app.listen(PORT, () => {
