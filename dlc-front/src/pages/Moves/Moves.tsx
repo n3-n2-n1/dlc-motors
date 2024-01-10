6cf6c91c442b158096fdfc151088608051516775
n3-n2-n1 <xiomaraengine@gmail.com>
2024-01-09 23:02:03 -0300

    formiks fixes

--- dlc-front/src/pages/Moves/Moves.tsx
+++ dlc-front/src/pages/Moves/Moves.tsx
@@ -26,11 +26,7 @@ const Moves: React.FC = () => {
             Historial de Movimientos
           </h1>
           <h2 className="text-gray-500 mb-4">
-            Visualizá sobre movimientos de inventario <br />
-            <span className="text-xs underline">
-              Lo usamos para arreglar el stock de las cosas que contamos y nos
-              dan mal
-            </span>
+            Visualizá sobre movimientos generales <br />
           </h2>
 		  </div>
 	  <div className="container h-full mx-auto px-4 sm:px-8 bg-gray-900">
@@ -40,46 +36,47 @@ const Moves: React.FC = () => {
               <table className="min-w-full leading-normal bg-gray-900">
                 <thead>
                   <tr>
-                    <th className="px-8 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                    <th className="px-8 py-3 border-b-2 border-gray-200 bg-gray-600 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider">
                       Fecha
                     </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-600 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider">
                       Descripción
                     </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-600 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider">
                       Accion
                     </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"></th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100"></th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-600 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider"></th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-600"></th>
                   </tr>
                 </thead>
                 <tbody>
                   {historial.map((move, index) => (
                     <tr tabIndex={index}>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm bg-gray-900">
+                      <td className="px-5 py-5 border-b border-gray-200 bg-gray-600 text-sm bg-gray-900">
                         <div className="flex">
                           <div className="ml-3">
-                            <p className="text-gray-900 whitespace-no-wrap">
+                            <p className="text-gray-200 whitespace-no-wrap">
                               {move.date}
                             </p>
                           </div>
                         </div>
                       </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <p className="text-gray-900 whitespace-no-wrap">
+                      <td className="px-5 py-5 border-b border-gray-200 bg-gray-600 text-gray-200 text-sm">
+                        <p className="text-gray-200 whitespace-no-wrap">
                           {move.observaciones}
                         </p>
                       </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <p className="text-gray-900 whitespace-no-wrap">
+                      <td className="px-5 py-5 border-b border-gray-200 bg-gray-600 text-gray-100 text-sm">
+                        <p className="text-gray-200 whitespace-no-wrap">
                         {move.movementType ? move.observaciones : move.descripcion}
                         </p>
                       </td>
-                      <td className=" border-b border-gray-200 bg-white text-sm"></td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">
+                      <td className=" border-b border-gray-200 bg-gray-600 text-sm text-gray-100">
+                      </td>
+                      <td className="px-5 py-5 border-b border-gray-200 bg-gray-600 text-gray-200 text-sm text-right">
                         <button
                           type="button"
-                          className="inline-block text-gray-500 hover:text-gray-700"
+                          className="inline-block text-gray-100 hover:text-gray-200"
                         >
                           <svg
                             className="inline-block h-6 w-6 fill-current"
