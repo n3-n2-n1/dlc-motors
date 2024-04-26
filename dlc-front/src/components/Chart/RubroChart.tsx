d05f12b1483a238a5604f5b011b1358c10175344
CRYSTAL <110715851+n3-n2-n1@users.noreply.github.com>
2024-04-26 01:43:22 -0300

    Chart fix to home - [FIX]

--- dlc-front/src/components/Chart/RubroChart.tsx
+++ dlc-front/src/components/Chart/RubroChart.tsx
@@ -21,54 +21,53 @@ interface ChartData {
 }
 
 const RubroChart = () => {
-  const { products } = useSearchContext();
-  const [chartData, setChartData] = useState<ChartData>({
-      labels: [],
-      datasets: []
-  });
+//   const { products } = useSearchContext();
+//   const [chartData, setChartData] = useState<ChartData>({
+//       labels: [],
+//       datasets: []
+//   });
 
-  useEffect(() => {
-    try {
-      if (products) {
-        const rubroCounts = products.reduce((acc, product) => {
-          acc[product.rubro] = (acc[product.rubro] || 0) + 1;
-          return acc;
-        }, {});
+//   useEffect(() => {
+//     try {
+//       if (products) {
+//         const rubroCounts = products.reduce((acc, product) => {
+//           acc[product.rubro] = (acc[product.rubro] || 0) + 1;
+//           return acc;
+//         }, {});
   
-        // Asegúrate de tomar solo 5 rubros (puedes ajustar esto según sea necesario)
-        const rubroLabels = Object.keys(rubroCounts).slice(0, 5);
-        const rubroData = Object.values(rubroCounts).slice(0, 5);
+//         // Asegúrate de tomar solo 5 rubros (puedes ajustar esto según sea necesario)
+//         const rubroLabels = Object.keys(rubroCounts).slice(0, 5);
+//         const rubroData = Object.values(rubroCounts).slice(0, 5);
   
-        setChartData({
-          labels: rubroLabels,
-          datasets: [
-            {
-              label: 'Rubro',
-              data: rubroData,
-              borderColor: 'rgba(93, 102, 235, 0.3)',
-              borderWidth: 1,
-              type: 'bar',
-              color: 'white' // Especifica el tipo de gráfico para este conjunto de datos
-            },
-            {
-              label: 'Tendencia',
-              data: rubroData, // Puedes cambiar estos datos para representar otra métrica si lo deseas
-              borderColor: 'rgba(255, 99, 132, 1)',
-              borderWidth: 2,
-              type: 'line' // Especifica el tipo de gráfico para este conjunto de datos
-            }
-          ]
-        });
-      }
-    } catch (error) {
-      console.error("Hubo un error al configurar los datos del gráfico: ", error);
-      // Aquí puedes manejar el error o mostrar un mensaje en la UI.
-    }
-  }, [products]);
+//         setChartData({
+//           labels: rubroLabels,
+//           datasets: [
+//             {
+//               label: 'Rubro',
+//               data: rubroData,
+//               borderColor: 'rgba(93, 102, 235, 0.3)',
+//               borderWidth: 1,
+//               type: 'bar',
+//               color: 'white' // Especifica el tipo de gráfico para este conjunto de datos
+//             },
+//             {
+//               label: 'Tendencia',
+//               data: rubroData, // Puedes cambiar estos datos para representar otra métrica si lo deseas
+//               borderColor: 'rgba(255, 99, 132, 1)',
+//               borderWidth: 2
+//             }
+//           ]
+//         });
+//       }
+//     } catch (error) {
+//       console.error("Hubo un error al configurar los datos del gráfico: ", error);
+//       // Aquí puedes manejar el error o mostrar un mensaje en la UI.
+//     }
+//   }, [products]);
   
 
   return <div className='w-[813px] h-[415px] p-4 rounded-xl dark:bg-gray-700 text-white'>
-      <Bar data={chartData} />
+      {/* <Bar data={chartData} /> */}
   </div>;
 };
 
