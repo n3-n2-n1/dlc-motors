0255d38fb941edff0c814352ea2c314edcf61de2
CRYSTAL <110715851+n3-n2-n1@users.noreply.github.com>
2024-06-12 16:12:19 -0300

    Rebranch cloudinary | Apolo 3.0.1

--- dlc-front/src/utils/cloudinaryTool.tsx
+++ dlc-front/src/utils/cloudinaryTool.tsx
@@ -1,8 +1,8 @@
-import CryptoJS from 'crypto-js';
+import CryptoJS from "crypto-js";
 
 export async function uploadImageToCloudinary(file) {
   if (!file) {
-    throw new Error('No se proporcionó un archivo.');
+    throw new Error("No se proporcionó un archivo.");
   }
 
   const apiKey = "75136931975783";
@@ -12,29 +12,37 @@ export async function uploadImageToCloudinary(file) {
 
   const timestamp = Math.round(new Date().getTime() / 1000);
   const signaturePayload = `timestamp=${timestamp}&upload_preset=${uploadPreset}`;
-  const signature = CryptoJS.SHA1(signaturePayload + apiSecret).toString(CryptoJS.enc.Hex);
+  const signature = CryptoJS.SHA1(signaturePayload + apiSecret).toString(
+    CryptoJS.enc.Hex
+  );
 
   const formData = new FormData();
-  formData.append('file', file);
-  formData.append('timestamp', timestamp.toString());
-  formData.append('api_key', apiKey);
-  formData.append('upload_preset', uploadPreset);
-  formData.append('signature', signature);
+  formData.append("file", file);
+  formData.append("timestamp", timestamp.toString());
+  formData.append("api_key", apiKey);
+  formData.append("upload_preset", uploadPreset);
+  formData.append("signature", signature);
 
   try {
-    const response = await fetch(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, {
-      method: 'POST',
-      body: formData,
-    });
+    const response = await fetch(
+      `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+      {
+        method: "POST",
+        body: formData,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      }
+    );
 
     if (!response.ok) {
-      throw new Error('Falló la carga de la imagen');
+      throw new Error("Falló la carga de la imagen");
     }
 
     const data = await response.json();
     return data.secure_url;
   } catch (error) {
-    console.error('Error al cargar la imagen:', error);
+    console.error("Error al cargar la imagen:", error);
     throw error;
   }
 }
