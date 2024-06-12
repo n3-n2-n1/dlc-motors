9ed03d5d41217486d202bf57efc2159b04ff2ea9
CRYSTAL <110715851+n3-n2-n1@users.noreply.github.com>
2024-06-12 16:26:21 -0300

    Rebranch cloudinary | Apolo 3.0.1

--- dlc-front/src/utils/cloudinaryTool.tsx
+++ dlc-front/src/utils/cloudinaryTool.tsx
@@ -2,47 +2,33 @@ import CryptoJS from "crypto-js";
 
 export async function uploadImageToCloudinary(file) {
   if (!file) {
-    throw new Error("No se proporcionó un archivo.");
+    throw new Error('No se proporcionó un archivo.');
   }
 
   const apiKey = "75136931975783";
-  const uploadPreset = "ml_default";
-  const apiSecret = "Sv87nV_eizZlsN4lIxvP66fp09o";
+  const uploadPreset = "ml_default"; // Reemplaza con tu upload preset
   const cloudName = "dripemmbb"; // Reemplaza con tu nombre de Cloudinary
 
-  const timestamp = Math.round(new Date().getTime() / 1000);
-  const signaturePayload = `timestamp=${timestamp}&upload_preset=${uploadPreset}`;
-  const signature = CryptoJS.SHA1(signaturePayload + apiSecret).toString(
-    CryptoJS.enc.Hex
-  );
-
   const formData = new FormData();
-  formData.append("file", file);
-  formData.append("timestamp", timestamp.toString());
-  formData.append("api_key", apiKey);
-  formData.append("upload_preset", uploadPreset);
-  formData.append("signature", signature);
+  formData.append('file', file);
+  formData.append('upload_preset', uploadPreset); // Asegúrate de que el preset esté presente
+  formData.append('api_key', apiKey);
 
   try {
-    const response = await fetch(
-      `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
-      {
-        method: "POST",
-        body: formData,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
-      }
-    );
+    const response = await fetch(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, {
+      method: 'POST',
+      body: formData
+    });
 
     if (!response.ok) {
-      throw new Error("Falló la carga de la imagen");
+      const errorText = await response.text();
+      throw new Error(`Falló la carga de la imagen: ${errorText}`);
     }
 
     const data = await response.json();
     return data.secure_url;
   } catch (error) {
-    console.error("Error al cargar la imagen:", error);
+    console.error('Error al cargar la imagen:', error.message);
     throw error;
   }
 }
