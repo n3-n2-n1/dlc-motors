cc5bbb7334e629ff161d5c2a9431e8c2478cdba9
CRYSTAL <110715851+n3-n2-n1@users.noreply.github.com>
2024-09-05 19:23:12 -0300

    Layout for notif

--- dlc-front/src/components/Navbar/Navbar.tsx
+++ dlc-front/src/components/Navbar/Navbar.tsx
@@ -6,15 +6,21 @@ interface NavbarProps {
   subtitle: string;
 }
 
-const Navbar = () => {
+const Navbar: React.FC<NavbarProps> = ({ title, subtitle }) => {
   return (
-    <nav className="bg-gray-800 p-4 flex justify-between items-center">
-      <div className="text-white text-lg">Mi Aplicación</div>
-      <div className="flex items-center">
-        {/* Aquí insertamos la campanita en el Navbar */}
+    <div className="bg-light dark:bg-dark flex flex-col sm:flex-row justify-between items-center  transition-colors duration-300 select-none">
+      <div>
+        <h1 className="text-xl sm:text-2xl md:text-3xl font-semibold text-gray-700 dark:text-white">
+          {title}
+        </h1>
+        <h2 className="text-sm sm:text-base md:text-lg  dark:text-gray-300 text-gray-400 pb-4">
+          {subtitle}
+        </h2>
+      </div>
+      <div>
         <NotificationBell />
       </div>
-    </nav>
+    </div>
   );
 };
 
